484544031858df2696f95c0e9b065bec2e9dff79
merdi-dev <merdibangongo@gmail.com>
2025-11-13 03:29:45 +0100

    Commit 6 Navigation, Disposition, About, Projet...

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 Ce site web est mon Portfolio
 un site qui me présente et me permet d'exposé mes compétence et réalisation à travers le monde
 
+😊💻
+
 # Auteur
-Merdi Bangongo, développeur informatique
+Merdi Bangongo, développeur web
     
+# ![Capture d'écran](./portfolio-merdi.png)
--- js/main.js
+++ js/main.js
@@ -1,31 +1,48 @@
 
-/******************************************************** 
-*  Evenement au préchargement de la page
-*
-********************************************************/
-
-function prechargement() {
-
-  document.addEventListener("DOMContentLoaded", () => {
-    const preloader = document.getElementById("prechargement");
-    const content = document.querySelector(".content");
-
-    // Masquer le préchargeur avec effet fondu
-    preloader.classList.add("fade-out");
-
-    // Quand l'animation du préchargeur est terminée
-    preloader.addEventListener('transitionend', () => {
-      preloader.style.display = 'none';
-      // Afficher le contenu avec fondu
-      content.classList.add("visible");
-    });
-  });
-}
 
 /******************************************************** 
 *   Menu au formant mobile (Gestion de la responsivité )
 *
 ********************************************************/
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  // - Date du footer
+  document.getElementById("date").textContent = new Date().getFullYear();
+  // - Version
+  document.getElementById("auteur").textContent = "by merdi dev v1.0";
+
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector(".menu");
+  const menuIcon = document.getElementById("menuIcon");
+  const navLinks = document.querySelector(".nav-links");
+  const navbar = document.querySelector(".navbar");
+
+  menu.addEventListener('click', () => {
+    // toggle menu mobile
+    navLinks.classList.toggle('mobile-menu');
+    document.body.classList.toggle('no-scroll');
+    navbar.classList.remove('scroller');
+
+    /** Changer l'icône */
+    if (navLinks.classList.contains('mobile-menu')) {
+      // menu ouvert → croix
+      menuIcon.classList.remove('fa-reorder');
+      menuIcon.classList.add('fa-fa-solid');
+    } else {
+      // menu fermé → hamburger
+      menuIcon.classList.remove('fa-solid');
+      menuIcon.classList.add('fa-reorder');
+    }
+
+  }); //menuIcon.textContent = menuIcon.textContent === '✖' ? '☰' : '✖';
+})
+
+/* 
+document.addEventListener("DOMContentLoaded", () => {
+
 function menuBerger() {
   const menu = document.querySelector(".menu")
   const navLinks = document.querySelector(".nav-links")
@@ -35,12 +52,4 @@ function menuBerger() {
     document.body.classList.toggle('no-scroll');
   })
 }
-
-// - Date du footer
-document.getElementById("date").textContent = new Date().getFullYear();
-
-// - Version
-document.getElementById("auteur").textContent = "by merdi dev v1.0.0";
-
-prechargement()
-menuBerger()
+ */
